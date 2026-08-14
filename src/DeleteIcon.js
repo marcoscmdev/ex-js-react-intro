b87cb792489966e061fa53765af98e71833627fb
MarcosCM <castromirandamarcos@gmail.com>
2026-08-14 14:47:32 +0200

    feat: add 2 custom buttons

--- src/DeleteIcon.js
+++ src/DeleteIcon.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { TodoIcon } from "./TodoIcon";
-function DeleteIcon() {
+function DeleteIcon({onDelete}) {
     return (
         <TodoIcon
         type="delete"
-        color="red"   
+        color="gray"   
+        onClick={onDelete}
         />
     );
 }
